5e3f614941786d05cd3093ceea7896980208f4e6
agftech <agiraof@gmail.com>
2020-11-05 17:23:24 -0300

    :dizzy: style: add style to the legend component

--- src/components/Legend/index.tsx
+++ src/components/Legend/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
+import "./index.css";
+
 interface LegendProps {
 	legend: string;
 }
 export const Legend: React.FC<LegendProps> = (props) => {
 	const { legend } = props;
-	return <div>{legend}</div>;
+	return <div className='Legend'>{legend}</div>;
 };
