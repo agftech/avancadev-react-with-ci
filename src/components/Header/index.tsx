1aaec654f7ca45541dd44d216d8b4e589e1a9799
agftech <agiraof@gmail.com>
2020-11-05 15:50:09 -0300

    :dizzy: style: add style to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import "./index.css";
+
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = (props) => {
 	return (
 		<div className='Header'>
 			<h1>Hello Header</h1>
-			<hr></hr>
 		</div>
 	);
 };
