0a48ff4f4258a34c23af0d5dc68a3b399d4009a4
agftech <agiraof@gmail.com>
2020-11-05 16:55:17 -0300

    :package: feat: add API service to get quotes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { cryptoHttp } from "../../http";
 import { Coin } from "../Coin";
 import "./index.css";
 
@@ -16,6 +17,29 @@ const ALL_PRICES: Price = {
 
 export const Header: React.FC<HeaderProps> = (props) => {
 	const [prices, setPrices] = React.useState<Price>(ALL_PRICES);
+	React.useEffect(() => {
+		const intervals = Object.keys(ALL_PRICES).map((coin) => {
+			return setInterval(() => {
+				cryptoHttp.get(`price?fsym=${coin}&tsyms=BRL`).then((response) => {
+					setPrices((prevState) => {
+						if (prevState[coin].currentPrice === response.data.BRL) {
+							return prevState;
+						}
+						return {
+							...prevState,
+							[coin]: {
+								oldPrice: prevState[coin].currentPrice,
+								currentPrice: response.data.BRL,
+							},
+						};
+					});
+				});
+			}, 5000);
+		});
+		return () => {
+			intervals.forEach((interval) => clearInterval(interval));
+		};
+	}, []);
 	return (
 		<div className='Header'>
 			{Object.keys(prices).map((coin) => (
