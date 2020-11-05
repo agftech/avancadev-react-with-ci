3a716620501251ad58fcb90d0e80a7126928cd4f
agftech <agiraof@gmail.com>
2020-11-05 16:34:44 -0300

    :package: feat: add abstraction to add any coin

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,10 +4,29 @@ import "./index.css";
 
 interface HeaderProps {}
 
+interface Price {
+	[key: string]: { oldPrice: number; currentPrice: number };
+}
+
+const ALL_PRICES: Price = {
+	BTC: { oldPrice: 0, currentPrice: 0 },
+	LTC: { oldPrice: 0, currentPrice: 0 },
+	USD: { oldPrice: 0, currentPrice: 0 },
+};
+
 export const Header: React.FC<HeaderProps> = (props) => {
+	const [prices, setPrices] = React.useState<Price>(ALL_PRICES);
 	return (
 		<div className='Header'>
-			<Coin coin='BTC' oldPrice={0} currentPrice={10} />
+			{Object.keys(prices).map((coin) => (
+				<div onClick={() => {}}>
+					<Coin
+						coin={coin}
+						oldPrice={prices[coin].oldPrice}
+						currentPrice={prices[coin].currentPrice}
+					/>
+				</div>
+			))}
 		</div>
 	);
 };
